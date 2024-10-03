8079f11a5cf15c2dd84eabd7c3d918df3598ee9b
Philippe Neri Singizwa <neriwest20@gmail.com>
2024-10-03 19:02:38 -0400

    add allLinks

---
+++ src/components/page-compontents/side-nav/allLinks.js
@@ -0,0 +1,76 @@
+const allLinks = [
+    {
+        name: 'Sales',
+        children: [
+            {
+                name: 'My Sales',
+                url: '',
+            },
+            {
+                name: 'New Sale',
+                url: '',
+            },
+            {
+                name: 'My sales',
+                url: '',
+            },
+            {
+                name: 'My report',
+                url: '',
+            },
+            {
+                name: 'Employee report',
+                url: '',
+            },
+
+
+        ]
+    },
+    {
+        name: 'Campaigns',
+        children: [
+            {
+                name: 'Add Campaign',
+                url: '',
+            },
+            {
+                name: 'All Campaigns',
+                url: '',
+            },
+            
+        ]
+    },
+    {
+        name: 'Goals',
+        children: [
+            {
+                name: 'Add Goal',
+                url: '',
+            },
+            {
+                name: 'All Goals',
+                url: '',
+            },
+            {
+                name: 'My Goals',
+                url: '',
+            },
+        ]
+    },
+    {
+        name: 'Employees',
+        children: [
+            {
+                name: 'Add Employee',
+                url: '',
+            },
+            {
+                name: 'Full Report',
+                url: '',
+            },
+            
+        ]
+    }
+]
+
+export default allLinks
