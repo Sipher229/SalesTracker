58d2d2efec9176c7221039301ee75f66bbf5c782
Philippe Neri Singizwa <neriwest20@gmail.com>
2024-11-28 00:19:34 -0500

    modify api baseurl

--- src/components/utils/API-calling-functions/axiosInstance.js
+++ src/components/utils/API-calling-functions/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.PROD ? '': 'http://localhost:3000'
+const baseURL = import.meta.env.PROD ? '/api': 'http://localhost:3000/api'
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
