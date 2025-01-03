2d42d84c73793fd5e235c390198c5f68f2ddb699
Philippe Neri Singizwa <neriwest20@gmail.com>
2025-01-03 09:14:07 -0500

    Change name to SalesVerse

--- src/components/page-compontents/header/Logo.jsx
+++ src/components/page-compontents/header/Logo.jsx
@@ -4,8 +4,8 @@ function Logo(){
   return (
     <>
         <span className= {`bg-inherit size-36 w-40 h-16 flex flex-col justify-center items-center `}>
-            <span className={`roboto-bold text-3xl text-center text-myyellow`}>Weed Man</span>
-            <span className={`roboto-light text-sm text-center text-white`}>Sales Tracker</span>
+            <span className={`roboto-bold text-3xl text-center text-myyellow`}>SalesVerse</span>
+            <span className={`roboto-light text-sm text-center text-white`}>Weed Man</span>
         </span>
     </>
   )
