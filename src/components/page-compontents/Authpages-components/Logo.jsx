543e89e8971efaee627004a3dfcf95e7468d634b
Philippe Neri Singizwa <neriwest20@gmail.com>
2025-01-06 00:56:47 -0500

    replace weed man with winning mode !

--- src/components/page-compontents/Authpages-components/Logo.jsx
+++ src/components/page-compontents/Authpages-components/Logo.jsx
@@ -5,7 +5,7 @@ function Logo(){
       <>
           <span className= {`bg-mygreen-700 size-36 w-40 h-16 flex flex-col justify-center items-center `}>
               <span className={`roboto-bold text-3xl text-center text-myyellow`}>SalesVerse</span>
-              <span className={`roboto-light text-sm text-center text-white`}>Weed Man</span>
+              <span className={`roboto-light text-sm text-center text-white`}>Winning Mode</span>
           </span>
       </>
     )
